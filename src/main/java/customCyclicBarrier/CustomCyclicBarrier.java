0c7a3276b84f7d46ae3dc438bf8e27011e8e5307
Suraj <surajsinghrawat12345@gmail.com>
2017-07-10 00:20:37 +0530

    [SURAJ] customCyclicBarrier

---
+++ src/main/java/customCyclicBarrier/CustomCyclicBarrier.java
@@ -0,0 +1,64 @@
+package customCyclicBarrier;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Suraj on 7/9/2017.
+ */
+public class CustomCyclicBarrier {
+
+    private final long count;
+    private final Lock lock;
+    private final AtomicLong tempCount;
+    private Runnable runnable;
+    private Thread threadAction;
+    private boolean actionFlag;
+
+
+    public CustomCyclicBarrier(long count, Runnable runnable) {
+        this(count);
+        this.runnable = runnable;
+        threadAction = new Thread(runnable);
+
+    }
+
+    public CustomCyclicBarrier(long count) {
+        this.count = count;
+        lock = new ReentrantLock();
+        tempCount = new AtomicLong(0);
+        actionFlag = false;
+    }
+
+    public void await() {
+        System.out.println(Thread.currentThread().getName() + " got into Awaited");
+
+
+        tempCount.incrementAndGet();
+
+        lock.lock();
+        System.out.println(Thread.currentThread().getName() + " got Lock");
+
+        while (true) {
+            if (tempCount.get() == count) {
+                releaseLock();
+                if(actionFlag==false && runnable!=null){
+                    threadAction.start();
+                    actionFlag = true;
+                }
+                break;
+
+            }
+        }
+
+    }
+
+    private void releaseLock() {
+
+        System.out.println(Thread.currentThread().getName() + " got released");
+        lock.unlock();
+
+    }
+
+}
